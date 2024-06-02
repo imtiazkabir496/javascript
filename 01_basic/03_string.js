6857d804f9b6ac64727ecf16cb703add73295033
Imtiaz Kabir <100441548+imtiazkabir496@users.noreply.github.com>
2024-06-02 06:22:18 +0000

    string

---
+++ 01_basic/03_string.js
@@ -0,0 +1,29 @@
+const name = "Kabir"
+const subscriber = 50
+
+//console.log(`My channel name is ${name} and total subscriber ${subscriber}`)
+
+const myChannel = new String('MynewChannel')
+
+// console.log(myChannel[0])
+// console.log(myChannel.length)
+// console.log(myChannel.toUpperCase())
+
+// console.log(name.charAt(2))
+// console.log(name.indexOf('i'))
+
+// console.log(name.substring(0,4))
+// console.log(name.slice(-4,3))
+
+
+const newName = "c    Imtiaz Kabir     "
+
+// console.log(newName)
+// console.log(newName.slice())
+
+
+const url = "https://www.google.com"
+
+console.log(url.replace('www' , 'hi'))
+console.log(url.includes('www'))
+console.log(url.split('.'));
