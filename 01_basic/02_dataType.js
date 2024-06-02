35c3b6de7499f84200c5c90da2d409123cff4207
Imtiaz Kabir <100441548+imtiazkabir496@users.noreply.github.com>
2024-06-02 03:04:27 +0000

    data type

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,22 @@
+// data type
+// primitive and non primitive
+
+//primitive Number, Boolean, BigInt, null, symbol, string, undefiend
+//non-primitive function, array, objects
+
+const id = Symbol("123")
+const moreId = Symbol("123")
+
+// console.log(id == moreId);
+
+const myArray = ["Kabir", "Imtiaz", "Riya"]
+
+let myObj = {
+    name: "kabir",
+    age: 34,
+    city: "Kolkata"
+}
+
+let myFunction = function (){
+    console.log("Hello world")
+}
